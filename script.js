143e942ce403673b06aad47cd05b53729c59ea3e
aaron-might <aaronmighty7@gmail.com>
2021-09-16 19:56:25 -0400

    write the function getclassby rate

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function getMovies() {
             />
         <div class="movie-info">
             <h3>${title}</h3>
-            <span>${vote_average}</span>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
         </div>
     `;
 
@@ -29,5 +29,13 @@ async function getMovies() {
     })
     return respData;
 }
-
+function getClassByRate(vote){
+    if (vote> 8){
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange'
+    }else {
+        return 'red';
+    }
+}
 getMovies();
